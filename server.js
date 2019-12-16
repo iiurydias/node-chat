5bdec8095d7cf138b9528851eee353b9e69ac531
Iury <iiurydias@hotmail.com>
2019-12-15 22:13:38 -0300

    styling

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ io.on('connection', socket => {
     })
     socket.on('clearMessages', () => {
         messages = [];
-        socket.broadcast.emit("clearDiv");
     })
 });
 server.listen(process.env.PORT || 3000);
